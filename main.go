92101200dc9a220cbda2c472e1fcf09f26eff50c
jeseias <domingosjeseias@gmail.com>
2026-01-03 21:49:36 +0100

    feat: get user input

--- main.go
+++ main.go
@@ -1,17 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+  fmt.Print(prompt)
+  input, err := r.ReadString('\n')
+  return input, err
+}
 
 func main() {
-  myBill := newBill("James Bond")
+  // myBill := newBill("James Bond")
 
-  myBill.addItem("Rice", 2.99)
-  myBill.addItem("Coffee", 7.50)
+  // myBill.addItem("Rice", 2.99)
+  // myBill.addItem("Coffee", 7.50)
 
-  myBill.updateTip(4.26)
+  // myBill.updateTip(4.26)
 
-  myBill.updateName("new name")
+  // myBill.updateName("new name")
+  r := bufio.NewReader(os.Stdin)
 
-	fmt.Println(myBill)
+  value, _ := getInput("To whom does this bill belong too? ", r)
+
+  println(value)
+
 }
